581a239b836fa1c75e785b4e7b80112ae081c5f2
Konstantin Turkin <like69.kt@gmail.com>
2022-07-06 14:10:47 +0300

    Add project files.

---
+++ BookLibraryManager/Controllers/LibrariesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibraryManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LibrariesController : ControllerBase
+    {
+        private readonly ILogger<LibrariesController> _logger;
+        //private readonly ILibraryService _booksService;
+
+        public LibrariesController(ILogger<LibrariesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
